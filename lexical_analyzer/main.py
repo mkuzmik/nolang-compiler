0ca72f52a4917d32287e5d5b40246a90e34d35b0
Mateusz Kuźmik <mateusz.kuzmik1@gmail.com>
2018-03-27 14:21:59 +0200

    Basic file reading char by char added

--- lexical_analyzer/main.py
+++ lexical_analyzer/main.py
@@ -1,2 +1,11 @@
+FILENAME = "foo"
+FILEDIR = "../input_files/"
 
+with open(FILEDIR + FILENAME) as source:
+    while True:
+        char = source.read(1)
+        if not char:
+            print("End of file")
+            break
+        print("Read a character: " + char)
 
